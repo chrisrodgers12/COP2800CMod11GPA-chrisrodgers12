93e0ac04ea84b0de72a7ebc3d300deb6483b700d
Chris Rodgers <s3772221@students.fscj.edu>
2026-04-06 00:08:40 -0400

    Module 11 Assignment - Exceptions

---
+++ edu/fscj/cop2800c/penguin/InvalidBirdDataException.java
@@ -0,0 +1,13 @@
+// InvalidBirdDataException.java
+// Christopher Rodgers
+// 4/5/26
+// Handling Invalid Data for Object creation
+
+package edu.fscj.cop2800c.penguin;
+
+public class InvalidBirdDataException extends Exception {
+   public InvalidBirdDataException(String message) {
+      super(message);
+   }
+}
+
